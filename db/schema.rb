c6d0a8edb086369cd8be80df322398f4569dae00
SirriRyisa <sirriryisa@gmail.com>
2022-08-15 16:17:09 +0100

    Created model for reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_15_141424) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_151231) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_141424) do
     t.index ["user_id"], name: "index_cities_on_user_id"
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.date "date"
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -51,4 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_15_141424) do
 
   add_foreign_key "cars", "users"
   add_foreign_key "cities", "users"
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
