6628f3261d1cfcab2202f04578a99c6e20792425
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-16 08:02:38 +0300

    set sessions and registration routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: {
+      registrations: :registrations,
+      sessions: :sessions,
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
