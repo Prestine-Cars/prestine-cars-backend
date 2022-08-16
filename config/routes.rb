aa63e99407a0c40692f8e863227bfe334b46f1c6
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-16 08:06:04 +0300

    Generate users controller and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers: {
-      registrations: :registrations,
-      sessions: :sessions,
-    }
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
