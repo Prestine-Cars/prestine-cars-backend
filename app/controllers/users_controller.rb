5113178370c649b304df5ea829d63f7f0812c522
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-16 08:08:26 +0300

    Generate users index and create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render json: @users
   end
 
   def create
+    @user = User.new(user_params)
+    return render json: @user.errors, status: :unprocessable_entity unless @user.save
+
+    render json: @user, status: :created
   end
 
   def show
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
