ed219008003e894d86806ba3e2ae724cd90b02b4
selma <belhadjselma1@gmail.com>
2022-08-17 11:32:52 +0100

    add the has_many reservation in the car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   belongs_to :city
+  has_many :reservations
 
   validates :model, presence: true, length: { in: 3..50 }
   validates :photo, presence: true
