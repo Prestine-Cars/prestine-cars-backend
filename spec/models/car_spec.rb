09870e78b6b8bc75d98b0d743babe081cc775122
selma <belhadjselma1@gmail.com>
2022-08-17 09:28:09 +0100

    Fix ruby offences

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   before(:each) do
     @user = User.create(name: 'Ben', email: 'ben@gmail.com', password: '123456')
-    @city = City.create(name: 'Berlin', flag_icon: 'www.example/germany.png', description: 'City in Germany', user: @user)
-    @car =  Car.create(model: 'Mercedes', photo: 'www.example/mercedez.png', description: 'This a german car', cost: 100, user: @user, city: @city)
+    @city = City.create(name: 'Berlin', flag_icon: 'www.example/germany.png', description: 'City in Germany',
+                        user: @user)
+    @car = Car.create(model: 'Mercedes', photo: 'www.example/mercedez.png', description: 'This a german car',
+                      cost: 100, user: @user, city: @city)
   end
 
   it 'is valid with valid attribute' do
--- spec/requests/cars_spec.rb
+++ spec/requests/cars_spec.rb
@@ -1,7 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Cars', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  #   let(:user) { User.create!(name: 'Amira', email: 'amira@gmail.com', password: '123456') }
+  #   let (:city) { City.create!(name: 'Berlin', flag_icon: 'www.example/germany.png',
+  # description: 'City in Germany', user: user) }
+
+  #   let(:valid_attributes) do
+  #     {
+  #       model: 'Mercedes',
+  #       photo: 'www.example/mercedez.png',
+  #       description: 'This a german car',
+  #       cost: 100,
+  #       user_id: user.id,
+  #       city_id: city.id
+  #     }
+  #   end
+
+  #   let (:invalid_attributes) do
+  #     {
+  #       model: '',
+  #       photo: '',
+  #       description: '',
+  #       cost: '',
+  #       user: '',
+  #       city: ''
+  #     }
+  #   end
+
+  #   describe 'POST /create' do
+  #     before { post cars_path, params: valid_attributes,
+  # headers: { 'Authorization' => AuthenticationTokenService.call(user.id) } }
+
+  #     context 'with valid parameters' do
+  #       it 'creates a new car' do
+  #         expect do
+  #          post cars_path, params: { car: valid_attributes }
+  #         end.to change(Car, :count).by(1)
+  #       end
+  #     end
+  #   end
 end
