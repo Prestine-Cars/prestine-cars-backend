ab28ebc3ce2414b4bf172b5278c2f8d6064c229d
Munezero Christian <chamsf5@aol.com>
2022-08-17 14:38:09 +0200

    Add routes for cities

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-   devise_for :users,
+  devise_for :users,
     controllers: {
       registrations: :registrations,
       sessions: :sessions,
     }
+
+  namespace :api do
+    namespace :v1 do
+      resources :cities, only: [:index, :show, :create, :destroy]
+    end
+  end
 end
