a80f4163e3d7ad29a56c7eb11bdcc125f897a2ae
Munezero Christian <chamsf5@aol.com>
2022-08-17 14:37:05 +0200

    Add city and car presenter

---
+++ app/representers/city_representer.rb
@@ -0,0 +1,19 @@
+class CityRepresenter
+  def initialize(city)
+    @city = city
+  end
+
+  def as_json
+    {
+      id: @city.id,
+      name: @city.name,
+      description: @city.description,
+      flag_icon: @city.flag_icon,
+      cars: @city.cars.map { |car| CarRepresenter.new(car).as_json }
+    }
+  end
+
+  private
+
+  attr_reader :user
+end
