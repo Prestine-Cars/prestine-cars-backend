48879a28639eff7a386f36913ef912f1a856e00d
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-23 13:54:51 +0300

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,19 +23,19 @@ cities = City.create!([
   {
     name: 'Nairobi',
     description: 'Nairobi is the capital of Kenya and the largest city in the country.',
-    flag_icon: 'https:example.com',
+    flag_icon: 'https://res.cloudinary.com/optprime/image/upload/v1661251766/Prestine%20Cars/kenya_kwjo7x.png',
     user: users.first,
   },
   {
     name: 'Kigali',
     description: 'Kigali is the capital of Rwanda and the largest city in the country.',
-    flag_icon: 'https:example.com',
+    flag_icon: 'https://res.cloudinary.com/optprime/image/upload/v1661251853/Prestine%20Cars/icons8-rwanda-96_lsw6c7.png',
     user: users.second,
   },
   {
     name: 'Algiers',
     description: 'Algiers is the capital of Algeria and the largest city in the country.',
-    flag_icon: 'https:example.com',
+    flag_icon: 'https://res.cloudinary.com/optprime/image/upload/v1661251852/Prestine%20Cars/icons8-algeria-96_sumpqa.png',
     user: users.first,
   }
 ])
@@ -44,7 +44,7 @@ cars = Car.create!([
   {
     model: 'Mercedes',
     description: 'German car',
-    photo: 'https:example.com',
+    photo: 'https://res.cloudinary.com/optprime/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1661251946/Prestine%20Cars/martin-katler-y3neNkE6efI-unsplash_axq2kh.jpg',
     cost: 24,
     city: cities.first,
     user: users.first,
@@ -52,7 +52,7 @@ cars = Car.create!([
   {
     model: 'Toyota',
     description: 'Japenese car',
-    photo: 'https:example.com',
+    photo: 'https://res.cloudinary.com/optprime/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1661251944/Prestine%20Cars/bestami-sarikaya-DwNmXuRb1Tk-unsplash_bmjwtm.jpg',
     cost: 10,
     city: cities.first,
     user: users.first,
