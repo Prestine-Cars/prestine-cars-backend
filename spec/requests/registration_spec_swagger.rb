91f5cc4d31437f973e078b5663e1a87d53d98f40
SirriRyisa <sirriryisa@gmail.com>
2022-08-25 09:47:14 +0100

    Add spec swagger

--- spec/requests/registration_spec_swagger.rb
+++ spec/requests/registration_spec_swagger.rb
@@ -1,21 +1,24 @@
 require 'swagger_helper'
 
-RSpec.describe 'registration', type: :request do
-  path '/users' do
-    post('sign-up') do
-      tags 'Users'
-      consumes 'application/json'
-      parameter name: :users, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string },
-          email: { type: :string },
-          password: { type: :string }
-        },
-        required: %w[email password]
-      }
-      response(200, 'successful') do
-        let(:users) { { name: 'Ben', email: 'ben@gmail.com', password: '123456' } }
+RSpec.describe 'api/v1/reservations', type: :request do
+  path 'api/v1/reservations' do
+    get('list reservations') do
+      tags 'Reservations'
+      security [bearerAuth: {}]
+      response(200, 'Successful') do
+        let(:user) { { name: 'Ben', email: 'mailto:ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(401, 'Not authorized') do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -28,21 +31,98 @@ RSpec.describe 'registration', type: :request do
     end
   end
 
-  path '/users' do
-    post('sign-up') do
-      tags 'Users'
+  path '/api/v1/cities/{city_id}/cars/{car_id}/reservations/' do
+        post('create reservation') do
+      tags 'Reservations'
+      security [bearerAuth: {}]
       consumes 'application/json'
-      parameter name: :users, in: :body, schema: {
+      parameter name: :reservation, in: :body, schema: {
         type: :object,
         properties: {
-          name: { type: :string },
-          email: { type: :string },
-          password: { type: :string }
+          user_id: { type: :integer },
+          car_id: { type: :integer },
+          date: { type: :string }
         },
-        required: %w[email password]
+        required: %w[user_id car_id date]
       }
 
-      response(422, 'Name/Email already exists') do
+      response(201, 'successful') do
+        let(:user) { { name: 'Ben', email: 'mailto:ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+        let(:reservation) { { user_id: 1, car_id: 1, date: '2022-01-01' } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(400, 'Bad Request') do
+        let(:user) { { name: 'Ben', email: 'mailto:ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+        let(:reservation) { { user_id: 1, car_id: 1 } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(401, 'Not authorized') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/cities/{city_id}/cars/{car_id}/reservations/{id}' do
+    parameter name: 'city_id', in: :path, type: :integer, description: 'city_id'
+    parameter name: 'car_id', in: :path, type: :integer, description: 'car_id'
+    parameter name: 'reservation_id', in: :path, type: :integer, description: 'reservation_id'
+    delete('delete reservation') do
+      tags 'Reservations'
+      security [bearerAuth: {}]
+      response(204, 'Reservation canceled') do
+        let(:id) { '1' }
+         let(:user) { { name: 'Ben', email: 'mailto:ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(401, 'You will need to login first') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(404, 'Record not found') do
+        let(:id) { '123' }
+         let(:user) { { name: 'Ben', email: 'mailto:ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
