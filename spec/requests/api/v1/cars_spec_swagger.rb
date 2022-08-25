14f8e50592d50592ff510d7b3d6942e6c5ba9587
selma <belhadjselma1@gmail.com>
2022-08-25 09:44:33 +0100

    Add rswag specs to post a car for a city

--- spec/requests/api/v1/cars_spec_swagger.rb
+++ spec/requests/api/v1/cars_spec_swagger.rb
@@ -29,5 +29,63 @@ RSpec.describe 'api/v1/cars', type: :request do
         run_test!
       end
     end
+    post('add a car for a city') do
+      tags 'City cars'
+      security [bearerAuth: {}]
+      consumes 'application/json'
+      parameter name: :reservation, in: :body, schema: {
+        type: :object,
+        properties: 
+        {
+      model:  { type: :string },
+      photo:  { type: :string },
+      description:  { type: :string },
+      cost:  { type: :integer },
+      user_id:  { type: :integer },
+      city_id: { type: :integer }
+    },
+        required: %w[model photo cost user_id city_id]
+      }
+      response(201, 'Created') do
+        let(:user) { { name: 'Ben', email: 'ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+         let(:city) { { name: 'Berlin', flag_icon: 'www.example/germany.png', description: 'City in Germany' } }
+        let(:car){ { model: 'Mercedes', photo: 'www.example/mercedez.png', description: 'This a german car',
+                      cost: 100, user_id: 1, city_id: 1} }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+      response(400, 'Bad Request') do
+        let(:user) { { name: 'Ben', email: 'ben@gmail.com', password: '123456' } }
+        let(:Authorization) { "Bearer #{AuthenticationTokenService.call(user.id)}" }
+         let(:city) { { name: 'Berlin', flag_icon: 'www.example/germany.png', description: 'City in Germany' } }
+        let(:car){ { photo: 'www.example/mercedez.png', description: 'This a german car',
+                      cost: 100, user_id: 1, city_id: 1} }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+      response(401, 'Not authorized') do
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
   end
 end
